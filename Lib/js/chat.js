6e041bdd8fae4a23512c057b25b6c601bba47750
Miguel <dev3apes@gmail.com>
2022-10-19 10:05:01 +0100

    update

---
+++ Lib/js/chat.js
@@ -0,0 +1,25 @@
+$.ajax({
+  type: "GET",
+  url: "../Lib/php/sender.php",
+  data: userDetails,
+
+  success: function (response) {
+    console.log(response);
+  },
+});
+
+const userDetails = {
+  name: "miguel",
+  lastName: "orfao",
+  email: "orfao.miguel@gmailicom",
+};
+
+$.ajax({
+  type: "POST",
+  url: "../Lib/php/chat.php",
+  data: userDetails,
+
+  success: function (response) {
+    console.log(response);
+  },
+});
